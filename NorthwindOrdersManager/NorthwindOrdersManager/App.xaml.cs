9e94fc6eaf0aa5f243ab70a948ae130a7a10efc5
Thomas  "Tom" Burchell <w0516036@nscc.ca>
2026-03-15 12:44:34 -0300

    IA4 initial

---
+++ NorthwindOrdersManager/NorthwindOrdersManager/App.xaml.cs
@@ -0,0 +1,8 @@
+using System.Windows;
+
+namespace NorthwindOrdersManager
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ NorthwindOrdersManager/NorthwindOrdersManager/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Windows;
+
+namespace NorthwindOrdersManager
+{
+    public partial class MainWindow : Window
+    {
+        // Existing code remains unchanged  
+    }
+}
